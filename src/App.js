327fa32549310bb74f9fc4e64c8e7d23f2ddabb4
Sweta Purbiya <jaiswal.sweta@gmail.com>
2020-08-06 18:26:04 -0700

    Sign-in style and page added, functionality missing

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component.jsx';
 import ShopPage from './pages/shop/shop.component.jsx';
 import Header from './components/header/header.component.jsx';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx';
 
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
       <Switch>
         <Route exact path='/' component= {HomePage} />
         <Route path='/shop' component= {ShopPage} />
+        <Route path='/signIn' component= {SignInAndSignUpPage} />
       </Switch>
     </div>
   );

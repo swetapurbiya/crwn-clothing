36c094ec0bd905053d3e0f14925cdcb590916dbc
Sweta Purbiya <jaiswal.sweta@gmail.com>
2020-09-04 16:06:26 -0700

    updated Stripe integration for payment and added favicon

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -4,6 +4,7 @@ import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 
 import CheckoutItem from "../../components/checkout-item/checkout-item.component";
+import StripeCheckoutButton from '../../components/stripe-button/stripe-button.component';
 
 import {
   selectCartItems,
@@ -39,6 +40,12 @@ const CheckoutPage = ({ cartItems, total }) => (
     <div className="total">
       <span>TOTAL : ${total}</span>
     </div>
+    <div className='test-warning'>
+      * Please use following test credit card details for payment *
+      <br />
+      4242 4242 4242 4242 - Expiry : 01/21 - CVV : 123
+    </div>
+    <StripeCheckoutButton price={total} />
   </div>
 );
 
